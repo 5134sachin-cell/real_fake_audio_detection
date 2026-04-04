fe1a8c67b0b6f30851a9ac3c8440f01f0a993c0b
sakthi <ssakthi17853@gmail.com>
2026-04-04 14:26:35 +0530

    added detail

--- app.py
+++ app.py
@@ -9,15 +9,15 @@ if __name__ == "__main__":
     runpy.run_path("app/app.py", run_name="__main__")
 
 """
+specific training:
 .\.venv\Scripts\Activate.ps1
 python -m src.training.train_cnn --epochs 3 --max_real_files 1000 --max_fake_files 1000
 
-
+full training:
 .\.venv\Scripts\Activate.ps1
 python -m src.training.train_cnn --epochs 20
 
-
-
+medium training:
 .\.venv\Scripts\Activate.ps1
 python -m src.training.train_cnn --epochs 10 --max_real_files 3000 --max_fake_files 3000
 
